f284fd8ffed8a49a7aec0c477a7ca28dd14d01ce
alone-guts <shauryaagarwal630@gmail.com>
2025-10-12 18:18:52 +0530

    Update main.py

--- --WAIFU--/main.py
+++ --WAIFU--/main.py
@@ -1,18 +1,18 @@
-from TEAMZYRO import *
+from WAIFU import *
 import importlib
 import logging
-from TEAMZYRO.modules import ALL_MODULES
+from WAIFU.modules import ALL_MODULES
 
 
 def main() -> None:
     for module_name in ALL_MODULES:
-        imported_module = importlib.import_module("TEAMZYRO.modules." + module_name)
-    LOGGER("TEAMZYRO.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        imported_module = importlib.import_module("WAIFU.modules." + module_name)
+    LOGGER("WAIFU.modules").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
 
-    ZYRO.start()
+    WAIFU.start()
     application.run_polling(drop_pending_updates=True)
-    LOGGER("TEAMZYRO").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY TEAMZYRO☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    LOGGER("WAIFU").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎MADE BY WAIFU☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     send_start_message()
     
